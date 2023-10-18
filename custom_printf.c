26674299fb62f1f81e5f66993cb0a201c875f431
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-10-18 01:16:45 +0100

    The completed file- custom_printf.c

---
+++ custom_printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * custom_printf - A custom printf function that handles %c, %s, %% specifiers.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int custom_printf(const char *format, ...)
+{
+	int chars_printed = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			chars_printed += output_char(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				chars_printed += output_char(va_arg(args, int));
+			} else if (*format == 's')
+			{
+				chars_printed += output_str(va_arg(args, char *));
+			} else if (*format == 'd' || *format == 'i')
+			{
+				chars_printed += write_to_console(va_arg(args, int));
+			} else if (*format == '%')
+			{
+				chars_printed += output_char('%');
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (chars_printed);
+
+}
